f4953317aeec9fb26d3ca6a5e04fd1827336d5c1
ACReturns <DSXtreme2012@gmail.com>
2024-07-16 23:05:48 -0700

    Assignment
    - updated SumArrayObject with a function overload

--- Assignments/SumArrayObject/SumArrayObject/Main.cpp
+++ Assignments/SumArrayObject/SumArrayObject/Main.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int sumArray(array<int, 10> theArray);
+void sumArray(array<int, 10> theArray, int& storeSum);
 
 int main()
 {
+	int sumReturn;
 	array<int, 10> primaryArray{ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 	int theResult = sumArray(primaryArray);
+	sumArray(primaryArray, sumReturn);
+
 	cout << "The result is " << theResult << endl;
+	cout << "The result is " << sumReturn << endl;
 
 	return 0;
 }
@@ -23,4 +28,9 @@ int sumArray(array<int, 10> theArray)
 	}
 
 	return sum;
+}
+
+void sumArray(array<int, 10> theArray, int& storeSum)
+{
+	storeSum = sumArray(theArray);
 }
